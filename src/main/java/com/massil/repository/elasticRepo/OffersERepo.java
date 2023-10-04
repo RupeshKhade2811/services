b2cb51554a44116310674c06099ab32e5ea83edc
rupesh.khade <rupesh.k@massiltech.com>
2023-10-04 18:42:53 +0530

    offers hibernate search

--- src/main/java/com/massil/repository/elasticRepo/OffersERepo.java
+++ src/main/java/com/massil/repository/elasticRepo/OffersERepo.java
@@ -1,28 +1,19 @@
 package com.massil.repository.elasticRepo;
 
-import com.google.gson.JsonObject;
 import com.massil.constants.AppraisalConstants;
 import com.massil.dto.CardsPage;
-import com.massil.persistence.model.EAppraiseVehicle;
 import com.massil.persistence.model.EOffers;
 import com.massil.util.CompareUtils;
-import org.hibernate.search.backend.elasticsearch.ElasticsearchExtension;
-import org.hibernate.search.backend.elasticsearch.search.aggregation.impl.ElasticsearchTermsAggregation;
-import org.hibernate.search.engine.search.aggregation.AggregationKey;
 import org.hibernate.search.engine.search.aggregation.SearchAggregation;
 import org.hibernate.search.engine.search.query.SearchResult;
-import org.hibernate.search.mapper.orm.scope.SearchScope;
 import org.hibernate.search.mapper.orm.session.SearchSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
+
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.UUID;
 
 @Component
@@ -66,9 +57,6 @@ public class OffersERepo {
         log.info("From ElasticSearchRepo liquidationCards");
 
         Integer offset=Math.multiplyExact(pageNumber,pageSize);
-
-
-
         SearchResult<EOffers> searchResult = searchSession.search(EOffers.class)
                 .where( f -> f.bool()
                         .must( f.match().field( "sellerUserId.id" )
@@ -82,13 +70,8 @@ public class OffersERepo {
                         .must(f.match().field("isTradeBuy")
                                 .matching(false))
                               )
-                .aggregation( countsByGenreKey, scope.aggregation().withRoot("appRef").terms()
-                        .field( "id", Object.class )
-                        .orderByTermDescending().toAggregation() )
                 .sort(f -> f.field( "modifiedOn" ).desc() )
                 .fetch(offset,pageSize);
-        Map<Object, Long> countsByGenre = searchResult.aggregation( countsByGenreKey );
-        List<Object> objectList = countsByGenre.keySet().stream().toList();
         long totalRecords = searchResult.total().hitCount();
         List<EOffers> liquidVehicles = searchResult.hits();
         CardsPage cardsPage=new CardsPage();
