ab6e7be55949b1128b040010de932315d4f1cd58
vijay <vijay.p@massiltech.com>
2023-11-24 12:01:27 +0530

    servePdf added

--- src/main/java/com/massil/services/impl/AppraiseVehicleServiceImpl.java
+++ src/main/java/com/massil/services/impl/AppraiseVehicleServiceImpl.java
@@ -1724,6 +1724,13 @@ public class AppraiseVehicleServiceImpl implements AppraiseVehicleService {
     }
 
     @Override
+    public byte[] servePdf(Long apprId) throws IOException {
+        EAppraiseVehicle appraisalById = eAppraiseVehicleRepo.getAppraisalById(apprId);
+        //object from amazons3
+        return utils.fileDownloadfromBucket(pdfpath, appraisalById.getTdStatus().getKeyAssureFiles());
+    }
+
+    @Override
     public byte[] previewPdf(String name) throws IOException {
         //object from amazons3
         return utils.fileDownloadfromBucket(pdfpath,name);
